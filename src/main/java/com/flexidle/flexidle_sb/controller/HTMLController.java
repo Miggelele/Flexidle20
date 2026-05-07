18166d9ede908289d4063f472389155840bf222e
elinpelin01 <elinpiho1@gmail.com>
2026-05-07 20:18:10 +0200

    Created the controller for html mappings

---
+++ src/main/java/com/flexidle/flexidle_sb/controller/HTMLController.java
@@ -0,0 +1,14 @@
+package com.flexidle.flexidle_sb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//en kontroller för att sköta mappings osv
+@Controller
+public class HTMLController {
+
+    @GetMapping("/")
+    public String index()  {
+        return "index";
+    }
+}
